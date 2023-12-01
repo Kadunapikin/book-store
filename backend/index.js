bf5974c0f1b70ead951bad83ade135589dd36eaf
kadunapikin <ibakoshi27@gmail.com>
2023-12-01 21:57:06 +0100

    create a get route to display welcome to MERN STACK tutorial

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log(`welcome to MERN-STACK tutorial`);
+    return res.status(234).send('Welcome to MERN STACK tutorial')
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
 })
