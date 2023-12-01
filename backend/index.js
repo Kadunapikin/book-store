6fbfec6d235b565e9300ec35015bc7d04c385fb4
kadunapikin <ibakoshi27@gmail.com>
2023-12-01 21:46:30 +0100

    create a basic server to listen at PORT 5555

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,8 @@
-const express = require('express');
+import express from 'express';
+import { PORT } from './config.js';
 
 const app = express();
+
+app.listen(PORT, () => {
+    console.log(`App is listening to port: ${PORT}`);
+})
