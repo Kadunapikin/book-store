24b418d98761e46edc9fb32cc593ebcb90c54bc1
kadunapikin <ibakoshi27@gmail.com>
2023-12-02 11:12:59 +0100

    create a /book route

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mangoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
+
+app.post('/book', (req, res) => {
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishYear) {
+            return res.status(400).send({ message: 'Send all required field: title, author and publishYear'});
+        }
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
 
 app.get('/', (req, res) => {
     console.log(`welcome to MERN-STACK tutorial`);
