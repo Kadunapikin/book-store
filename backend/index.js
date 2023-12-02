d5551b50b81a77d020be408f49c83a8737359f24
kadunapikin <ibakoshi27@gmail.com>
2023-12-02 11:42:07 +0100

    return the new created book as reponse to user

--- backend/index.js
+++ backend/index.js
@@ -10,17 +10,20 @@ app.post('/book', async (req, res) => {
         if(!req.body.title || !req.body.author || !req.body.publishYear) {
             return res.status(400).send({ message: 'Send all required field: title, author and publishYear'});
         }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        };
+
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+                
     } catch (error) {
         console.log(error.message);
         res.status(500).send({ message: error.message });
     }
 });
-
-const newBook = {
-    title: req.body.title,
-    author: req.body.author,
-    publishYear: req.body.publishYear
-};
 
 
 app.get('/', (req, res) => {
