3332e7386a6c40b2c02c5d8a186696e12c4a66cb
kadunapikin <ibakoshi27@gmail.com>
2023-12-03 14:29:17 +0100

    create a route to update a specific book on the database

--- backend/index.js
+++ backend/index.js
@@ -59,6 +59,25 @@ app.get('/books/:id', async (req, res) => {
     }
 });
 
+//Route to update a book
+app.put('/books/:id', async (req,res) => {
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishYear) {
+            return res.status(400).send({ message: 'Send all required field: title, author and publishYear'});
+        }
+        const { id } = req.params;
+        const result = await Book.findByIdAndUpdate(id, req.body);
+
+        if(!result) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        return res.status(200).json({ message: 'Book updated successfully' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: error.message });
+    }
+});
+
 
 mangoose.connect(mongoDBURL)
 .then(() => {
