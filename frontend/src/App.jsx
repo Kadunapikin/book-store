7c1bb551b17659669c39caa636cfe722ee64e400
kadunapikin <ibakoshi27@gmail.com>
2023-12-04 20:11:57 +0100

    import Routes and Route and create 5 differend Route on  App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,13 @@ import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>
-      App is working fine
-    </div>
+    <Routes>
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+    </Routes>
   )
 }
 
