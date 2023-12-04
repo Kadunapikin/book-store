0c7b7806cae4bda558a2e58543226567c3438549
kadunapikin <ibakoshi27@gmail.com>
2023-12-04 20:54:42 +0100

    create a useState to handle books and loading state on Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
-import { AiOuttlineEdit } from 'react-icons/ai';
+import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
@@ -10,6 +10,8 @@ import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setIsloading] = useState(false);
 
   return (
     <div>Home</div>
