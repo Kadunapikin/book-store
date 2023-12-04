97055af63841412e3cfe60104bd199815fad879e
kadunapikin <ibakoshi27@gmail.com>
2023-12-04 20:50:26 +0100

    import axios and other Icons from react-icons on Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { AiOuttlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+
+
+
 
 const Home = () => {
+
   return (
     <div>Home</div>
   )
