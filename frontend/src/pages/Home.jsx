1d4f79cf37b0192a3da4bca9caea0749f56b80c8
kadunapikin <ibakoshi27@gmail.com>
2023-12-04 21:46:46 +0100

    add a h1 tag to show Books List with a link to /books/create

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -31,7 +31,14 @@ const Home = () => {
   }, []);
 
   return (
-    <div>Home</div>
+    <div className='p-4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>Books List</h1>
+        <Link to='/books/create'>
+          <MdOutlineAddBox className='text-slate-800 text-4xl' />
+        </Link>
+      </div>
+    </div>
   )
 }
 
