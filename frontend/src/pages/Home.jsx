9d57674d5aa234bc48818efcd08770541059d1c4
kadunapikin <ibakoshi27@gmail.com>
2023-12-04 21:09:52 +0100

    add a useEffect to get data from the database

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,24 @@ import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 const Home = () => {
   const [books, setBooks] = useState([]);
-  const [loading, setIsloading] = useState(false);
+  const [loading, setLoading
+  ] = useState(false);
+
+  useEffect(() => {
+    setLoading
+    (true);
+    axios
+    .get('http://localhost:5555/books')
+    .then((response) => {
+      setBooks(response.data.data);
+      setLoading
+      (false);
+    })
+    .catch((error) => {
+      console.log(error);
+      setLoading(false);
+    })
+  }, []);
 
   return (
     <div>Home</div>
