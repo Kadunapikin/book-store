5a0dcfb171d9d9e5223f4b628e6c5b96336daedb
kadunapikin <ibakoshi27@gmail.com>
2023-12-04 21:48:04 +0100

    add a function to return Spinner.jsx component when isLoading and to return tables of books when its not

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -38,6 +38,22 @@ const Home = () => {
           <MdOutlineAddBox className='text-slate-800 text-4xl' />
         </Link>
       </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <table className='w-full border-separate border-spacing-2'>
+          <thead>
+            <tr>
+              <th className='border border-slate-600 rounded-md'>No</th>
+              <th className='border border-slate-600 rounded-md'>Title</th>
+              <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
+              <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
+              <th className='border border-slate-600 rounded-md'>Operations</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      )}
     </div>
   )
 }
