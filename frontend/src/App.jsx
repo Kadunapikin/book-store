36480a0a8b0c9156c287eeb343c1fc446ff7cb12
kadunapikin <ibakoshi27@gmail.com>
2023-12-04 23:45:42 +0100

    add a map function on books to return title, author, publish year and opertions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,11 @@ import DeleteBook from './pages/DeleteBook'
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={Home} />
-      <Route path='/books/create' element={CreateBook} />
-      <Route path='/books/edit/:id' element={EditBook} />
-      <Route path='/books/details/:id' element={ShowBook} />
-      <Route path='/books/delete/:id' element={DeleteBook} />
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
     </Routes>
   )
 }
