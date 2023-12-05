61553c7e745cce3c815979e0789ca3908f190936
kadunapikin <ibakoshi27@gmail.com>
2023-12-05 13:08:34 +0100

    add loading logic to display book details when setLoading is false

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -24,7 +24,40 @@ const ShowBook = () => {
   }, [])
 
   return (
-    <div>ShowBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Show Book</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className='flex flex-col border-2 border-sky-400 rounded-xl min-w-full p-4'>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Id</span>
+            <span>{books._id}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Title</span>
+            <span>{books.title}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Author</span>
+            <span>{books.author}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
+            <span>{books.publishYear}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Create Time</span>
+            <span>{new Date(books.createdAt).toString()}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Last Update Time</span>
+            <span>{new Date(books.updatedAt).toString()}</span>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
