4b2c0bc00ea9e5321371b2fe8501fa4ebf5a8ca5
kadunapikin <ibakoshi27@gmail.com>
2023-12-05 21:27:20 +0100

    add jsx tag to accept user input for title and handle title onChange state

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -25,14 +25,29 @@ const CreateBook = () => {
     })
     .catch((error) => {
       console.log(error);
-      alert('An error happened, please check the console')
+      alert('An error happened, please check the console');
       setLoading(false);
     })
 
   }
 
   return (
-    <div>CreateBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Create Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <input 
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
