72e1c562fde92b70e845d9a58d9389d6e9425e35
kadunapikin <ibakoshi27@gmail.com>
2023-12-05 12:24:58 +0100

    add items to BackButton.jsx component

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BsArrowLeft } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
-const BackButton = () => {
+const BackButton = ({destination = '/'}) => {
   return (
-    <div>BackButton</div>
+    <div className='flex'>
+        <Link
+        to={destination}
+        className='bg-sky-800 text-white rounded-lg px-4 py-1 w-fit '
+        >
+        <BsArrowLeft className='text-2xl' />
+        </Link>
+    </div>
   )
 }
 
