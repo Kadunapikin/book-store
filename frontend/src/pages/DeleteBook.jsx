254791c9c253b067c1ad8c6f07f5c317c3950361
kadunapikin <ibakoshi27@gmail.com>
2023-12-05 22:53:55 +0100

    import components to use on DeleteBook.jsx component

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import BackButton from '../components/BackButton';
 
 const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+  const { id } = useParams();
+
   return (
     <div>DeleteBook</div>
   )
