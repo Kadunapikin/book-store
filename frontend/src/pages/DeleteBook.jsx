28a94d180c62e974dd7b0e7cdc5984d4e1f43f8f
kadunapikin <ibakoshi27@gmail.com>
2023-12-05 23:21:00 +0100

    add jsx tag and a button to handleDeleteBook onClick

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -24,7 +24,15 @@ const DeleteBook = () => {
   }
 
   return (
-    <div>DeleteBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Delete Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+        <h3 className='text-2xl'>Are you sure you want to delete this book?</h3>
+        <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeleteBook}>Yes Delete this book!</button>
+      </div>
+    </div>
   )
 }
 
