c8520652a4a0230dd21b6970f9ff19c67733f077
kadunapikin <ibakoshi27@gmail.com>
2023-12-05 21:24:02 +0100

    create a useState on CreateBook.jsx component to handle all the states

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import BackButton from '../components/BackButton';
 
 const CreateBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+  
+
   return (
     <div>CreateBook</div>
   )
